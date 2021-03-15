fcb155c52363cc314848905aa02bd5b221694a06
RobertObi <robobi97@googlemail.com>
2021-03-15 16:34:02 -0500

    commit tree 81cd0c2ecd257111e371fcb990033be83b753dbf

--- app.rb
+++ app.rb
@@ -1,21 +1,16 @@
 class App < Sinatra::Base
 
-	get '/' do
-		erb :index
-	end
-
 	get '/hello' do
 		erb :hello
-	end 
+	end
 
 	get '/goodbye' do
-		erb :goodbye
-	end 
-
-	get '/date' do
-		erb :date
-		# x = Date.new.now
+		erb :goobye
 	end
+
+# 	get '/date' do
+# 		erb :date
+# 	end
 
 
 
